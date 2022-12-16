1e41149a374e10487b94ed2ed1155874fb993697
byungnam <byungnam.baek@dabeeo.com>
2022-12-16 14:23:06 +0900

    ✨feat: change name hello

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'HI' });
+  res.render('index', { title: 'HELLO' });
 });
 
 module.exports = router;
